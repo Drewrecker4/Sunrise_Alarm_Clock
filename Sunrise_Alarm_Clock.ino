8318df81ab63a4be3ad4521e10a79d352af478c5
Drewrecker4 <86975139+Drewrecker4@users.noreply.github.com>
2024-04-13 22:18:54 -0400

    cleanup variables

--- Sunrise_Alarm_Clock.ino
+++ Sunrise_Alarm_Clock.ino
@@ -8,73 +8,74 @@
 #include "IRremote.h"
 
 //  I/O Mapping
-const byte  red_led     = 6;
-const byte  blue_led    = 5;
-const byte  green_led   = 7;
-const byte  receiver    = 33;
-const byte  oled_cs     = 53;
-const byte  oled_dc     = 2;
-const byte  oled_reset  = 48;
-const byte  row_1       = 22;
-const byte  row_2       = 23;
-const byte  row_3       = 24;
-const byte  row_4       = 25;
-const byte  col_1       = 26;
-const byte  col_2       = 27;
-const byte  col_3       = 28;
-const byte  col_4       = 29;
-const byte  dfplayer_tx = 19;
-const byte  dfplayer_rx = 18;
-const byte  busy_pin    = 8;
-const byte  skip_btn    = 30;
-const byte  snooze_btn  = 31;
-const byte  snooze_lt   = 32;
-const byte  vol_up_btn  = 34;
-const byte  vol_dn_btn  = 35;
+const uint8_t   oled_dc     = 2;
+const uint8_t   blue_led    = 5;
+const uint8_t   red_led     = 6;
+const uint8_t   green_led   = 7;
+const uint8_t   busy_pin    = 8;
+const uint8_t   dfplayer_rx = 18;
+const uint8_t   dfplayer_tx = 19;
+const uint8_t   row_1       = 22;
+const uint8_t   row_2       = 23;
+const uint8_t   row_3       = 24;
+const uint8_t   row_4       = 25;
+const uint8_t   col_1       = 26;
+const uint8_t   col_2       = 27;
+const uint8_t   col_3       = 28;
+const uint8_t   col_4       = 29;
+const uint8_t   skip_btn    = 30;
+const uint8_t   snooze_btn  = 31;
+const uint8_t   snooze_lt   = 32;
+const uint8_t   receiver    = 33;
+const uint8_t   vol_up_btn  = 34;
+const uint8_t   vol_dn_btn  = 35;
+const uint8_t   oled_reset  = 48;
+const uint8_t   oled_cs     = 53;
 
 //  Initialize Variables
-//void initialize_variables() {
-  //  LED Strip
-  const byte  max_brightness      = 255;
-  byte        green_sp            = 30;
-  byte        red_sp              = 255;
-  byte        blue_sp             = 0;
-  byte        green_brightness    = 0;
+//  Global Variables
+//  LED Strip
+const uint8_t   max_brightness      = 255;
+uint8_t         green_sp            = 30;
+uint8_t         red_sp              = 255;
+uint8_t         blue_sp             = 0;
+uint8_t         green_brightness    = 0;
+uint8_t         red_brightness      = 0;
+uint8_t         blue_brightness     = 0;
 
-  //  RTC Module
-  char        days_of_week[7][12] = {"Sunday",
-                                     "Monday",
-                                     "Tuesday",
-                                     "Wednesday",
-                                     "Thursday",
-                                     "Friday",
-                                     "Saturday"};
-  byte        fixed_minutes;
-  byte        fixed_hours;
-  byte        fixed_seconds;
-  word        fixed_year;
-  byte        fixed_month;
-  byte        fixed_day;
-  char        fixed_day_of_week;
-  bool        meridiem;
+//  RTC Module
+char    days_of_week[7][12] = { "Sunday",
+                                "Monday",
+                                "Tuesday",
+                                "Wednesday",
+                                "Thursday",
+                                "Friday",
+                                "Saturday"};
+uint8_t fixed_minutes;
+uint8_t fixed_hours;
+uint8_t fixed_seconds;
+word    fixed_year;
+uint8_t fixed_month;
+uint8_t fixed_day;
+char    fixed_day_of_week;
+bool    meridiem;
 
-  //  Keypad Module
-  const byte  rows  = 4;
-  const byte  cols  = 4;
-  char        keys[rows][cols] = {{'1','2','3','A'},
-                                   {'4','5','6','B'},
-                                   {'7','8','9','C'},
-                                   {'*','0','#','D'}};
-  byte        row_pins[rows]    = {row_1, row_2, row_3, row_4};
-  byte        col_pins[cols]    = {col_1, col_2, col_3, col_4};
-  String      key_input;
-  String      remote_input;
+//  Keypad Module
+const uint8_t rows  = 4;
+const uint8_t cols  = 4;
+char          keys[rows][cols] = {{'1','2','3','A'},
+                                 {'4','5','6','B'},
+                                 {'7','8','9','C'},
+                                 {'*','0','#','D'}};
+uint8_t       row_pins[rows]    = {row_1, row_2, row_3, row_4};
+uint8_t       col_pins[cols]    = {col_1, col_2, col_3, col_4};
+String        key_input;
+String        remote_input;
 
-  // MP3 Module
-  byte        random_song;
-  bool        busy_state;
-  bool        skip_state;
-//}
+// MP3 Module
+uint8_t     random_song;
+bool        busy_state;
+bool        skip_state;
 
 //  Library Structs
 IRrecv          irrecv(receiver);
@@ -86,10 +87,15 @@ DFRobotDFPlayerMini myDFPlayer;
 
 void setup()
 {
-  //initialize_variables();
+  initialize_variables();
 }
 
 void loop()
 {
   
+}
+
+void initialize_variables()
+{
+
 }
